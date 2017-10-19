9e2b17b7de0089289beb591c5c9fe78d5581763a
Felipe Duzzi <duzzifelipe@gmail.com>
2017-10-19 19:57:23 -0200

    poison

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule ZohomailApi.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpotion, "~> 3.0.2"}
+      {:httpotion, "~> 3.0.2"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
